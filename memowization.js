a4ecdd7a63fb3413e92bf1e756f54aac52c72507
NoahNim <nmedoff123@gmail.com>
2021-05-22 15:58:29 -0700

    yes yes fastfib given uwu

--- memowization.js
+++ memowization.js
@@ -33,3 +33,34 @@ console.log(lucasNumberMemo(1))  // => 1
 console.log(lucasNumberMemo(40))  // => 228826127
 console.log(lucasNumberMemo(41))  // => 370248451
 console.log(lucasNumberMemo(42))  // => 599074578
+
+/***********************************************************************
+Write a recursive function called `fibonacci` that takes an integer, `n`,
+and returns the `n`th number in the Fibonacci sequence.
+
+Not familiar with the Fibonacci sequence? Beginning with 0 and 1, we add the two
+previous numbers in the sequence together to form the next one:
+
+0, 1, 1, 2, 3, 5, 8, etc....
+
+We count Fibonacci numbers beginning with the first 1. Take a look at the
+examples below if you're unsure where to start!
+
+Examples:
+
+fastFib(1); // 1
+fastFib(2); // 1
+fastFib(3); // 2
+fastFib(4); // 3
+fastFib(10); // 55
+***********************************************************************/
+
+function fastFib(n, memo = {}) {
+
+}
+
+console.log(fastFib(1)); // 1
+console.log(fastFib(2)); // 1
+console.log(fastFib(3)); // 2
+console.log(fastFib(4)); // 3
+console.log(fastFib(10)); // 55
