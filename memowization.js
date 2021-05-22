9f5da3957756330cad3802e6b37a1273f1e6462c
Sunny Mallick <sunny.mallick927@gmail.com>
2021-05-22 16:47:28 -0600

    added problem

---
+++ memowization.js
@@ -0,0 +1,24 @@
+/*********************************************************************
+Write a function, lucasNumberMemo(n), that takes in a number.
+The function should return the n-th number of the Lucas Sequence.
+The 0-th number of the Lucas Sequence is 2.
+The 1-st number of the Lucas Sequence is 1
+To generate the next number of the sequence, we add up the previous two
+numbers.
+
+For example, the sequence begins: 2, 1, 3, 4, 7, 11, ...
+
+Solve this recursively with memoization.
+
+Examples:
+
+lucasNumberMemo(0)   // => 2
+lucasNumberMemo(1)   // => 1
+lucasNumberMemo(40)  // => 228826127
+lucasNumberMemo(41)  // => 370248451
+lucasNumberMemo(42)  // => 599074578
+*********************************************************************/
+
+function lucasNumberMemo(n, memo = {}) {
+    // your code here
+  }
