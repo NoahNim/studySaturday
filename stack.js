b69eae9b312ac89c3f015c759a9b209f4e0cc4ce
Sunny Mallick <sunny.mallick927@gmail.com>
2021-05-22 16:13:39 -0600

    completed a stack array

--- stack.js
+++ stack.js
@@ -1,3 +1,26 @@
 class Stack {
-
+    constructor() {
+        this.store = new Array();
+    }
+    push(ele) {
+        this.store.push(ele)
+        return true;
+    }
+    pop() {
+        return this.store.pop();
+    }
+    size() {
+        return this.store.length;
+    }
+    peek() {
+        return this.store[this.store.length - 1]
+    }
 }
+
+let stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.pop()
+console.log(stack);
+console.log(stack.size())
+console.log(stack.peek())
